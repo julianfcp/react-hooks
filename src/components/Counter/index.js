d7d5153efbdf08817ead2a4acb34763851f1ff63
Julian <julian.fcp@gmail.com>
2022-04-11 01:29:39 -0500

    first commit react hooks

---
+++ src/components/Counter/index.js
@@ -0,0 +1,45 @@
+import { useReducer } from "react";
+import "../../estilos.css";
+
+const initialState = () => ({
+  count: 0,
+});
+
+const reducer = (state = initialState(), action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case "RESET":
+      return {
+        ...state,
+        count: 0,
+      };
+    default:
+      return state;
+  }
+};
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState());
+
+  return (
+    <div className="Counter">
+      <h2>Contador useReducer</h2>
+
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>RESET</button>
+    </div>
+  );
+}
+
+export default Counter;
